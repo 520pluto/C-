f4eb4d4b3caeb76e736d5157ea0737e1a05881b2
蛋蛋Sir <98202270+520pluto@users.noreply.github.com>
2022-01-26 15:16:02 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,34 +1,31 @@
-#define _CRT_SECURE_NO_WARNINGS
-
 #include<stdio.h>
-int main()
-{
-	int a, b, c, d;
-	printf("输入任意三个数字：");
-	scanf("%d%d%d", &a, &b, &c);
-	if (a < b)
-	{
-		d = a; a = d; b = d;
-	}
-	if (a < c)
-	{
-		d= a; a = c; c = d;
-	}
-	if (b < c)
-	{
-		d= b; b = c; c = d;
-	}
-	printf("由大到小排列的顺序为：%d,%d,%d\n", a, b, c);
-	//return 0;
-}
+# define   _CRT_SECURE_NO_WARNINGS
+//int Add(int x, int y)
+//{
+//	int z = 0;
+//    z = x + y;
+//	return z;
+//}
+//
 //int main()
 //{
-//	int a, b, o;
-//	printf("输入两个参数：");
-//	scanf("%d%d", &a, &b);
-//	if (a < b)
-//	{
-//		o = a; a = b; b = o;
-//		printf("输入参数由大到小排列：%d，%d\n", a, b);
-//	}
+//	int a = 15;
+//	int b = 24;
+//    int sum=Add(a, b);
+//	return 0;
 //}
+int main()
+{
+	int age = 18;
+	int weight;
+	int height = 170;
+	scanf("%d",&weight);
+	if (weight > 130)
+	{
+		printf("管住嘴迈开腿");
+	}
+	else if (weight < 110)
+	{
+		printf("您太瘦啦，好好吃饭");
+	}
+}
